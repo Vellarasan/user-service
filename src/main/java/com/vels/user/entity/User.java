bcaf3a7fe176ec1f1a41e85a4f0132ef3034bd0e
vdhanapathy <vdhanapathy@ntst.com>
2021-10-03 12:51:53 +0530

    Initial commit for user service

---
+++ src/main/java/com/vels/user/entity/User.java
@@ -0,0 +1,26 @@
+package com.vels.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date Dob;
+    private Long departmentId;
+}
